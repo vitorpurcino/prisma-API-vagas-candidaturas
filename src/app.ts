2e6fb0706a73bb078e3f26b942c43c6ce710dd69
vitorpurcino <vitor.purcino@gmail.com>
2024-05-06 08:44:45 -0400

    correção packaje.json

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import "express-async-errors";
 import "reflect-metadata";
 import "dotenv/config";
+import cors from "cors";
 import express, { json } from "express";
 import helmet from "helmet";
 import { opportunityRouter } from "./routes/opportunity.routes";
 import { userRoutes } from "./routes/users.routes";
 import { HandleErrors } from "./middlewares/handleErrors.middlewares";
-import cors from "cors";
 
 export const app = express();
 
